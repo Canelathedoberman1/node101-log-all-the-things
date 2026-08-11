219c849b2860cad6b5cb54a8251a5c59d5566bc1
Canelathedoberman <eduardo.deleon@cccareers.org>
2026-08-11 13:59:56 -0700

    Complete logger assignment

--- server/app.js
+++ server/app.js
@@ -3,18 +3,55 @@ const fs = require('fs');
 const app = express();
 
 app.use((req, res, next) => {
-// write your logging code here
+  const agent = req.get('User-Agent');
+  const time = new Date().toISOString();
+  const method = req.method;
+  const resource = req.originalUrl;
+  const version = `HTTP/${req.httpVersion}`;
+  const status = 200;
 
+  const log = `${agent},${time},${method},${resource},${version},${status}\n`;
+
+  console.log(log);
+
+  fs.appendFile('./server/log.csv', log, (err) => {
+    if (err) {
+      console.error(err);
+    }
+  });
+
+  next();
 });
 
 app.get('/', (req, res) => {
-// write your code to respond "ok" here
-
+  res.status(200).send('ok');
 });
 
 app.get('/logs', (req, res) => {
-// write your code to return a json object containing the log data here
+  fs.readFile('./server/log.csv', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send('Unable to read log file');
+    }
 
+    const lines = data.trim().split('\n');
+    const headers = lines.shift().split(',');
+
+    const logs = lines.map((line) => {
+      const values = line.split(',');
+
+      return {
+        Agent: values[0],
+        Time: values[1],
+        Method: values[2],
+        Resource: values[3],
+        Version: values[4],
+        Status: values[5]
+      };
+    });
+
+    res.json(logs);
+  });
 });
 
 module.exports = app;
